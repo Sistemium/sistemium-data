7296a9249841c320468c13ae1b88442e6891be4f
Alexander Levin <alevin.ru@gmail.com>
2021-08-01 08:44:38 +0300

    export settle

--- src/util/axios.js
+++ src/util/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
+import coreSettle from 'axios/lib/core/settle';
 
 export default axios.create();
 
 export function axiosInstance(config = {}) {
   return axios.create(config);
 }
+
+export const settle = coreSettle;
