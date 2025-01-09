8d01b7f1d2d86bdfc850c3e2f1e3d287b11597a9
Alexander Levin <alevin.ru@gmail.com>
2025-01-09 13:56:08 +0200

    predicate matcher

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { default as StoreAdapter } from './StoreAdapter';
 export { default as CommonFieldsPlugin } from './plugins/CommonFieldsPlugin';
 export * from './Model';
 export { AxiosResponse } from 'axios';
+export { default as matcher } from './util/predicates';
